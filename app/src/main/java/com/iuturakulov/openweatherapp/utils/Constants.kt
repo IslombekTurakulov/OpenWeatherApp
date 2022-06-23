8cfa6b68de368ee2e0a9fa1a61d374feb3587d1b
IslombekTurakulov <iuturakulov@edu.hse.ru>
2022-06-23 17:27:49 +0300

    Bug fix

--- app/src/main/java/com/iuturakulov/openweatherapp/utils/Constants.kt
+++ app/src/main/java/com/iuturakulov/openweatherapp/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.iuturakulov.openweatherapp.utils
 
 val CITY_KEY: String = "CITY_KEY"
 val THEME_KEY: String = "THEME_KEY"
+const val STORAGE_NAME = "appStorage"
 const val ROOM_DB_NAME = "ROOM_DB_NAME"
